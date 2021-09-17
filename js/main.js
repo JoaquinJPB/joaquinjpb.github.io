2945e26e3df4cce44149ff64c24336fdd905bfe4
JoaquinJPB <jjpb2000@gmail.com>
2021-09-17 14:32:26 +0100

    Dark/Light Theme

--- js/main.js
+++ js/main.js
@@ -87,3 +87,27 @@ let swiper = new Swiper(".portfolio_container", {
         clickable: true,
     },
 });
+
+/* ************ DARK/LIGHT THEME ************ */
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+/*==================== DARK LIGHT THEME ====================*/  
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
